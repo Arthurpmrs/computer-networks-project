f5a9cd31bf3cb24f77501a73a272a5cdab3e6a7b
Arthurpmrs <arthurpmrs@gmail.com>
2024-03-08 20:30:44 -0300

    draft: add input to get message

--- src/client.py
+++ src/client.py
@@ -2,7 +2,7 @@ from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 
 
 def udp_client():
-    server_name = '192.168.0.42'
+    server_name = '192.168.0.36'
     port = 12000
 
     n = 36
@@ -23,23 +23,28 @@ def udp_client():
 
 
 def tcp_client():
-    server_name = '192.168.0.42'
-    port = 12000
+    server_name = '192.168.0.36'
+    server_port = 12000
 
-    n = 12
+    n = input("Digite um número para ser elevado ao quadrado: ")
 
     message = str(n).encode()
 
     client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((server_name, port))
+    client_socket.connect((server_name, server_port))
 
+    print(f"Requesting to server (ip={server_name}, port={server_port})...")
     client_socket.send(message)
     response = client_socket.recv(1024)
 
-    print(f"Sending address: {client_socket.getsockname()}")
-    print(f"Server Answer: {response.decode()}")
+    # print(f"Server Answer: {response.decode()}")
+    # print(f"Sending address: {client_socket.getsockname()}")
 
+    print("Response received. Closing conection...\n")
     client_socket.close()
+
+    ans = response.decode()
+    print(f"Sua resposta é: {ans}")
 
 
 if __name__ == "__main__":
